565ea9e4b0a3f155fcb7e8b4df5e187662eaf940
jameslafontaine <79965281+jameslafontaine@users.noreply.github.com>
2024-11-03 17:09:40 +1100

    Complete exercises 01, 06-11
    Four new exercises were added to the upstream repository, they are
    exercises 02-05. So the old exercises 02-07 were changed to be
    exercises 06-11.
    
    This commit simply recognizes this change in exercise ordering. As far
    as I can remember, the new exercises were originally completed by me on
    Replit, so I will not be completing them again here.

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,17 @@
-const removeFromArray = function() {
+const removeFromArray = function (array, ...exclude) {
+    result = []
+
+    for (let i = 0; i < array.length; i++) {
+        // Copy elements from the input array to the result array excluding
+        // the values provided in the function call
+        if (!(exclude.includes(array[i]))) {
+            result.push(array[i])
+        }
+    }
+
+    // console.log(result)
+
+    return result
 };
 
 // Do not edit below this line
