39609a610eb8581372b2b9825ea6f3ee23ddf0dc
jameslafontaine <79965281+jameslafontaine@users.noreply.github.com>
2024-11-03 17:07:32 +1100

    Complete exercises 01-07

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,7 +1,17 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (temp) {
+  conversion = ((temp - 32) * 5 / 9)
+
+  rounded = Math.round(conversion * 10) / 10;
+
+  return rounded
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (temp) {
+  conversion = (temp * 9 / 5 + 32)
+
+  rounded = Math.round(conversion * 10) / 10;
+
+  return rounded
 };
 
 // Do not edit below this line
