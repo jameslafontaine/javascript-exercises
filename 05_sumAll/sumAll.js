dfcbc2bd35b21d0164871aba985b4f0e99ca1994
jameslafontaine <79965281+jameslafontaine@users.noreply.github.com>
2024-11-03 17:09:40 +1100

    Complete exercises 01-07

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,23 @@
-const sumAll = function() {
+const sumAll = function (num1, num2) {
+
+    // Input validation
+    if (num1 < 0 || num2 < 0 || !Number.isInteger(num1) ||
+        !Number.isInteger(num2)) {
+        return "ERROR"
+    }
+
+    start = Math.min(num1, num2)
+    end = Math.max(num1, num2)
+
+    sum = 0
+
+
+
+    for (let i = start; i <= end; i++) {
+        sum += i;
+    }
+
+    return sum;
 
 };
 
