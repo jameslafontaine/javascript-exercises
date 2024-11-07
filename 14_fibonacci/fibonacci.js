e5aba7a72e9d9e89327317be2136641d64d602fe
jameslafontaine <79965281+jameslafontaine@users.noreply.github.com>
2024-11-07 22:47:59 +1100

    Complete exercises 12-16
    These were originally known as exercises 08-12, but have been renamed
    to match the current numbering scheme due to the four added exercises.

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,5 +1,26 @@
-const fibonacci = function() {
+const fibonacci = function (num) {
 
+    if (num < 0) {
+        return "OOPS"
+    }
+
+    result = 0;
+
+    prev1 = 1;
+
+    prev2 = 1;
+
+    if (num == 1 || num == 2) {
+        return 1;
+    }
+
+    for (let i = 2; i < num; i++) {
+        result = prev1 + prev2;
+        prev1 = prev2;
+        prev2 = result;
+    }
+
+    return result;
 };
 
 // Do not edit below this line
